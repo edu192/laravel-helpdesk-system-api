1fbb873107c850baf36e835687c642e1970d99f2
edu <89234306+edu455@users.noreply.github.com>
2024-09-11 02:10:58 -0500

    Add api tokens to user model and add token response return in auth methods register and login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -7,26 +7,33 @@ use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Sanctum\PersonalAccessToken;
 
 class AuthenticatedSessionController extends Controller
 {
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): Response
+    public function store(LoginRequest $request)
+    : array
     {
         $request->authenticate();
 
         $request->session()->regenerate();
+        $token = \auth()->user()->createToken('auth_token');
 
-        return response()->noContent();
+        return ['token' => $token->plainTextToken];
     }
 
     /**
      * Destroy an authenticated session.
      */
-    public function destroy(Request $request): Response
+    public function destroy(Request $request)
+    : Response
     {
+        if ($token = PersonalAccessToken::where('tokenable_id', $request->user()->id)->first()) {
+            $token->delete();
+        }
         Auth::guard('web')->logout();
 
         $request->session()->invalidate();
--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -18,7 +18,7 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request): Response
+    public function store(Request $request): array
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
@@ -36,6 +36,8 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return response()->noContent();
+        $token = \auth()->user()->createToken('auth_token');
+
+        return ['token' => $token->plainTextToken];
     }
 }
