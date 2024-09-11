01a016bb996b695c729ddffe2768fbcd3f279b90
edu <89234306+edu455@users.noreply.github.com>
2024-09-11 02:59:07 -0500

    Add all policies from https://github.com/edu192/laravel-support-tickets-system.git

---
+++ app/Policies/TicketPolicy.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Ticket;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TicketPolicy
+{
+    use HandlesAuthorization;
+
+    public function before(User $user, string $ability)
+    : bool|null
+    {
+        if ($user->type === 0) {
+            return true;
+        }
+
+        return null;
+    }
+
+    public function viewAny(User $user)
+    : bool
+    {
+        return false;
+    }
+
+    public function view(User $user, Ticket $ticket)
+    : bool
+    {
+        return $user->id === $ticket->user_id;
+    }
+
+    public function create(User $user)
+    : bool
+    {
+    }
+
+    public function update(User $user, Ticket $ticket)
+    : bool
+    {
+    }
+
+    public function delete(User $user, Ticket $ticket)
+    : bool
+    {
+        return $ticket->status == 0 && $user->id === $ticket->user_id;
+    }
+
+    public function restore(User $user, Ticket $ticket)
+    : bool
+    {
+    }
+
+    public function forceDelete(User $user, Ticket $ticket)
+    : bool
+    {
+    }
+
+    public function post_comment(User $user, Ticket $ticket)
+    : bool
+    {
+        if ($ticket->status !== 2) {
+            $participantIds = $ticket->assigned_agent->pluck('id')->toArray();
+            if ($user->id === $ticket->user_id || in_array($user->id, $participantIds)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
